e6486ee222b66afdb5bc22351ca4364b12bfb4a1
amartya <62711821+amartyaay@users.noreply.github.com>
2022-04-04 14:20:51 +0530

    added apiCall to functions

--- newsdata/views.py
+++ newsdata/views.py
@@ -2,20 +2,10 @@ import imp
 from turtle import title
 from urllib import response
 from django.shortcuts import render, HttpResponse
-import requests
-from private import api_key
+from functions.api import apiCall
 from functions.summary import summarize
+from functions.sentiment import sentiment
 # Create your views here.
-
-
-def apiCall():
-
-    base_url = "https://newsdata.io/api/1/news?apikey=" + \
-        api_key+"&country=in&language=en"
-    # print(base_url)
-    response = requests.get(base_url)
-    json_data = response.json() if response and response.status_code == 200 else None
-    return json_data
 
 
 def index(request):
