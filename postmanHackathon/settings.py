6448e9f92dc36e943e2ce589ce4ef29b19cb483c
amartya <62711821+amartyaay@users.noreply.github.com>
2022-04-05 03:18:10 +0530

    increased allowed hosts

--- postmanHackathon/settings.py
+++ postmanHackathon/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&vm*lnnlcfa8acr06bccd507r3_$3f%5u=ua%)$fvtthdg3na9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.137.1']
 
 
 # Application definition
